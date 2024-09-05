c67285d93343c1ce430d98961517f506282de8de
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-09-05 15:33:04 +0900

    kadai_007 コミット

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>Kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 連想配列に値を代入する
+         $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160 ];
+ 
+         // 連想配列の値を出力する
+         print_r($onion_data);
+ 
+         ?>
+     </p>
+ </body>
+ 
+ </html>
